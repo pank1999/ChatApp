96993fbb307ad35092202127de19295e71e88425
Your Name <pankajpandey9589@gmail.com>
2022-08-21 22:14:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header/>
+        <div className="App">
+          <div className="left"> left</div>
+          <div className="center">center</div>
+          <div className="right">right</div>
+        </div>
     </div>
+   
   );
 }
 
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+        <h1>Chat App</h1>
+        <p>Create memborable chat</p>
+    </div>
+  )
+}
